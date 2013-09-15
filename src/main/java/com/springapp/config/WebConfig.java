1b5958a5b9646231d06419a61f9e785d898c95e9
Shekhar Gulati <shekhargulati84@gmail.com>
2013-09-15 12:28:51 +0900

    imported infra xml in webapp config

--- src/main/java/com/springapp/config/WebConfig.java
+++ src/main/java/com/springapp/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.springapp.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.springapp.controllers.IndexController;
@@ -9,6 +10,7 @@ import com.springapp.controllers.IndexController;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackageClasses = IndexController.class)
+@ImportResource("classpath:infrastructure.xml")
 public class WebConfig {
 
 }
